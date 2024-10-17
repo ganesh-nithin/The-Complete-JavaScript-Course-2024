c58c91097b6c743be3c3dd37d5311620c581156f
Ganesh Nithin <sarvabhotla.g@solitontech.com>
2024-10-17 20:39:39 +0530

    feat: add assignment 6.
    Signed-off-by: Ganesh Nithin <sarvabhotla.g@solitontech.com>

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+buttonShowModal.forEach(showModal => {
+  showModal.addEventListener('click', openModal);
+});
+
+buttonCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
